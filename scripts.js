04fb25bc44939c69274bd8de5eb0fdf2e9b76dca
Muhammad Jawad Ali <jimjawad57@gmail.com>
2024-07-24 23:26:42 +0500

    New design

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function sendMessage() {
+    var message = document.getElementById('message').value;
+    alert('Your message: ' + message);
+    
+
+}
+
+
+function playVideo() {
+    var video = document.getElementById('demo-video');
+    video.play();
+}
